9c25adc910d1a19d1bfbe465aca92080639a72aa
Dries <admin@dries007.net>
2022-08-18 22:16:22 +0200

    Fixes problem with locking & can msg calls, fixes #1 HA not responding to changes via switches.

--- custom_components/dobiss_can/light.py
+++ custom_components/dobiss_can/light.py
@@ -4,7 +4,7 @@ Documentation of protocol: https://gist.github.com/dries007/436fcd0549a52f26137b
 The CAN bus must be enabled by the system before this module is loaded.
 systemd-networkd can do this for you, as CAN is a supported network.
 
-Author: Dries007 - 2021
+Author: Dries007 2021 - 2022
 Licence: MIT
 """
 import asyncio
@@ -15,20 +15,28 @@ import can
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
 from homeassistant.components.light import LightEntity, PLATFORM_SCHEMA
-from homeassistant.const import CONF_HOST, CONF_LIGHTS, CONF_NAME
-from homeassistant.core import HomeAssistant
+from homeassistant.const import CONF_LIGHTS, CONF_NAME, EVENT_HOMEASSISTANT_STOP
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import DiscoveryInfoType, ConfigType
 
 
 _LOGGER = logging.getLogger(__name__)
 _LOGGER.setLevel(logging.DEBUG)
 
-CONF_BUSTYPE = 'bustype'
+DOMAIN = 'dobiss_can'
+
+CONF_INTERFACE = 'interface'
+CONF_CHANNEL = 'channel'
 CONF_MODULE = 'module'
 CONF_RELAY = 'relay'
 
+# Example:
 """
 light:
   - platform: dobiss_can
+    interface: socketcan  # optional
+    channel: can0         # optional
     lights:
       - name: WC
         module: 1
@@ -38,8 +46,8 @@ light:
         relay: 1
 """
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_HOST, default="can0"): cv.string,
-    vol.Optional(CONF_BUSTYPE, default="socketcan"): cv.string,
+    vol.Optional(CONF_INTERFACE, default="socketcan"): cv.string,
+    vol.Optional(CONF_CHANNEL, default="can0"): cv.string,
     vol.Required(CONF_LIGHTS): vol.All(cv.ensure_list, [vol.Schema({
         vol.Required(CONF_NAME): cv.string,
         vol.Required(CONF_MODULE): cv.positive_int,
@@ -48,27 +56,59 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 })
 
 
-async def async_setup_platform(hass: HomeAssistant, config, async_add_entities, discovery_info=None):
+async def async_setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    async_add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None,
+):
     # Load CAN bus. Must be operational already (done by external network tool).
     # Setting bitrate might work, but ideally that should also be set already.
     # We only care about messages related to feedback from a SET command or the reply from a GET command.
-    bus = can.Bus(channel=config[CONF_HOST], bustype=config[CONF_BUSTYPE], bitrate=125000, receive_own_messages=False, can_filters=[
+    # todo: ThreadSafeBus?
+    bus = can.Bus(bustype=config[CONF_INTERFACE], channel=config[CONF_CHANNEL], bitrate=125000, receive_own_messages=True, can_filters=[
         {"can_id": 0x0002FF01, "can_mask": 0x1FFFFFFF, "extended": True},  # Reply to SET
         {"can_id": 0x01FDFF01, "can_mask": 0x1FFFFFFF, "extended": True},  # Reply to GET
     ])
+
+    @callback
+    def stop(event):
+        bus.shutdown()
+
+    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, stop)
+
+    loop = asyncio.get_running_loop()
+
     # Global CAN bus lock, required since the reply to a GET does not include any differentiator.
+    # This means we must lock, then send out a GET request.
+    # The reply will then only be acked by the entity that holds the lock.
+    # I don't like this, it smells, but it works and IDK how to do it better.
     lock = asyncio.Lock()
 
     # All config entries get turned into entities that listen for updates on the bus.
     entities = [DobissLight(bus, o, lock) for o in config[CONF_LIGHTS]]
-    can.Notifier(bus, entities)
+    # can.Listener
+    can.Notifier(bus, (e.on_can_message_received for e in entities), loop=loop)
     async_add_entities(entities)
 
     # Success.
     return True
 
 
-class DobissLight(LightEntity, can.Listener):
+# noinspection PyAbstractClass
+class DobissLight(LightEntity):
+    _attr_has_entity_name = True
+    _attr_name = None
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {
+                (DOMAIN, self.unique_id)
+            },
+            "name": self._name,
+        }
+
     def __init__(self, bus: can.BusABC, o: dict, lock: asyncio.Lock):
         self._bus = bus
         self._lock = lock
@@ -87,11 +127,7 @@ class DobissLight(LightEntity, can.Listener):
         self._awaiting_update = False
         self._event_update = asyncio.Event()
         # Logger
-        self._log = _LOGGER.getChild(self.name)
-
-    @property
-    def name(self) -> str:
-        return self._name
+        self._log = _LOGGER.getChild(self._name)
 
     @property
     def is_on(self) -> bool:
@@ -113,7 +149,7 @@ class DobissLight(LightEntity, can.Listener):
     async def async_turn_off(self, **kwargs: Any) -> None:
         self._bus.send(can.Message(arbitration_id=self._set_id, data=self._bytes_off, is_extended_id=True), timeout=.1)
 
-    async def on_message_received(self, msg):
+    def on_can_message_received(self, msg: can.Message):
         # Reply to SET, this we can filter because data contains data from.
         if msg.arbitration_id == 0x0002FF01 and msg.data[0] == self._module and msg.data[1] == self._relay:
             self.is_on = msg.data[2] == 1
@@ -125,15 +161,17 @@ class DobissLight(LightEntity, can.Listener):
     async def async_update(self):
         # The update cycle must be blocked on the CAN bus lock.
         async with self._lock:
-            # Inform handler that we expect an update.
-            self._awaiting_update = True
-            # Small delay, otherwise we overload the CAN module.
-            await asyncio.sleep(.01)
-            # Ask CAN module for an update
-            self._bus.send(can.Message(arbitration_id=0x01FCFF01, data=self._bytes_status, is_extended_id=True), timeout=.1)
-            # Wait for reply to come
-            await self._event_update.wait()
-            # Small delay, otherwise we overload the CAN module.
-            await asyncio.sleep(.01)
-            # OK, we're done.
-            self._awaiting_update = False
+            try:
+                # Inform handler that we expect an update.
+                self._awaiting_update = True
+                # Small delay, otherwise we overload the CAN module.
+                await asyncio.sleep(.01)
+                # Ask CAN module for an update
+                self._bus.send(can.Message(arbitration_id=0x01FCFF01, data=self._bytes_status, is_extended_id=True), timeout=.1)
+                # Wait for reply to come
+                await asyncio.wait_for(self._event_update.wait(), 0.5)
+                # Small delay, otherwise we overload the CAN module.
+                await asyncio.sleep(.01)
+            finally:
+                # In all cases, no matter how we get out of this, we must unset the _awaiting_update flag.
+                self._awaiting_update = False
